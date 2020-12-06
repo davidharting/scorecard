4eb4513f2a8b722df7a313a4066aba0a9a1635b1
David Harting <david.e.harting@gmail.com>
2020-12-06 10:47:06 -0500

    get back to barebones

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Scorecard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Scorecard</h1>
+        <button>Create a new scorecard.</button>
+
+        <form>
+          <label htmlFor="roomCode">Join a room by entering a room code</label>
+          <input name="roomCode" type="text"></input>
+          <button type="submit">Join room</button>
+        </form>
+      </main>
+    </div>
+  );
+}
