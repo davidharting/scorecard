e98d6f205a2a5e9598d7b30cb87247f2337af3f1
David Harting <david.e.harting@gmail.com>
2020-12-06 11:05:51 -0500

    fix up ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { ReactComponentElement, ReactNode } from "react";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: MyAppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
+
+interface MyAppProps {
+  Component: React.FC;
+  pageProps: any;
+}
